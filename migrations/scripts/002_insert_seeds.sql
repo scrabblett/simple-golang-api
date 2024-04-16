e3c387363b0264dc9c579489cb5acebcbc4c7719
Dmitry Sagan <sagandmitry03@gmail.com>
2024-04-16 20:04:49 +0700

    fix migrations (added admin user in seeds)

--- migrations/scripts/002_insert_seeds.sql
+++ migrations/scripts/002_insert_seeds.sql
@@ -1,25 +1,23 @@
 -- +goose Up
-INSERT INTO public.age_groups (group_age, group_name)
+INSERT INTO age_groups (group_age, group_name)
 VALUES (6, '6+');
 
-INSERT INTO public.age_groups (group_age, group_name)
+INSERT INTO age_groups (group_age, group_name)
 VALUES (12, '12+');
 
-INSERT INTO public.age_groups (group_age, group_name)
+INSERT INTO age_groups (group_age, group_name)
 VALUES (18, '18+');
 
-INSERT INTO public.age_groups (group_age, group_name)
+INSERT INTO age_groups (group_age, group_name)
 VALUES (16, '16+');
 
 INSERT INTO library_user (last_name, first_name, patronymic, birth_date, created_at, edited_at, removed_at)
 VALUES ('admin', 'admin', 'admin', '01-01-1900', current_date, current_date, null);
 
 INSERT INTO users_info (login, password, salt, user_id)
-SELECT 'admin', '2e735fd45433813b166a4e7379d52948fd65f2f0215fcba32b26c60abe1f06e9', '5cf5546e4e19b43apass', id
-FROM public.library_user
+SELECT 'admin',
+       E'\\xD4D452001FDDD95B1921548C6536941B9C631158A33F981629A59590D6506D62',
+       E'\\xB45E72C2EBC963CC',
+       id
+FROM library_user
 WHERE first_name = 'admin';
-
-INSERT INTO users_tokens (token, expired_at, user_id)
-select '12345', '01-01-3000', user_id
-from users_info
-where login = 'admin';
